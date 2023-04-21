18c3b4d1e93d2d9625eb77a58a0e056004dfe2c0
Austin Wallace <austinwallace196@gmail.com>
2023-04-21 00:22:56 -0400

    HF complete, parallel begins

--- cpp/src/hf.cpp
+++ cpp/src/hf.cpp
@@ -1,7 +1,9 @@
 #include "helper.hpp"
 #include "input.hpp"
+#include "omp.h"
 #include "stdio.h"
 #include <Eigen/Dense>
+#include <ctime>
 #include <fstream>
 #include <iostream>
 #include <string>
@@ -9,7 +11,7 @@
 
 using namespace std;
 
-int main() {
+void serial() {
   // Specify Data Path
   std::string dataPath = "data";
   double t1 = 1e-8, t2 = 1e-8;
@@ -52,36 +54,34 @@ int main() {
   H = new Eigen::MatrixXd(T->rows(), T->cols());
   *H = *T + *V;
   /* *H = *T + *V + *e1; */
-  cout << endl << "H Matrix: " << endl << endl << *H << endl;
+  /* cout << endl << "H Matrix: " << endl << endl << *H << endl; */
 
   // Orthogonalize Basis Set
   S_12 = new Eigen::MatrixXd(S->rows(), S->cols());
   helper::orthoS(S, S_12);
-  cout << endl << "S_12 Matrix: " << endl << endl << *S_12 << endl;
+  /* cout << endl << "S_12 Matrix: " << endl << endl << *S_12 << endl; */
 
   // Build initial Fock Matrix
   F = new Eigen::MatrixXd(H->rows(), H->cols());
   *F = (*S_12).transpose() * *H * (*S_12);
-  cout << endl << "F Matrix: " << endl << endl << *F << endl;
+  /* cout << endl << "F Matrix: " << endl << endl << *F << endl; */
 
   C_0_prime = new Eigen::MatrixXd(H->rows(), H->cols());
   helper::getC_0_prime(F, C_0_prime);
-  cout << endl << "C_0_prime Matrix: " << endl << endl << *C_0_prime << endl;
+  /* cout << endl << "C_0_prime Matrix: " << endl << endl << *C_0_prime << endl; */
 
   C = new Eigen::MatrixXd(H->rows(), H->cols());
   *C = (*S_12) * (*C_0_prime);
-  cout << endl << "C Matrix: " << endl << endl << *C << endl;
+  /* cout << endl << "C Matrix: " << endl << endl << *C << endl; */
 
   D = new Eigen::MatrixXd(H->rows(), H->cols());
   helper::updateDensityMatrix(C, D, num_electrons);
-  cout << endl << "D Matrix: " << endl << endl << *D << endl;
+  /* cout << endl << "D Matrix: " << endl << endl << *D << endl; */
 
-  helper::SCF(
-          eri,
-          S_12, H, F, C, D, C_0_prime, num_electrons, &E, e_nuc, t1, t2
-          );
+  helper::SCF(eri, S_12, H, F, C, D, C_0_prime, num_electrons, &E, e_nuc, t1,
+              t2);
   E += e_nuc;
-  cout << endl << "Final HF Energy: " << E  << endl;
+  cout << endl << "Final HF Energy: " << E << endl;
 
   // E_0_elec -125.84207743769902
 
@@ -98,5 +98,29 @@ int main() {
   /* free(D); */
   /* free(S_12); */
   printf("\nFreed Memory\n");
+}
+
+int main() {
+  int num_threads = 4;
+  omp_set_num_threads(1);
+  Eigen::setNbThreads(1);
+  time_t start, end;
+  double serial_t, omp_t;
+  start = clock();
+  serial();
+  end = clock();
+  serial_t = (double)(end - start);
+  cout << "Serial Time: " << (serial_t / CLOCKS_PER_SEC) << endl;
+  /* Serial Time: 0.01617 */
+  omp_set_num_threads(num_threads);
+  Eigen::setNbThreads(num_threads);
+  start = clock();
+  serial();
+  end = clock();
+  omp_t = (double)(end - start);
+  cout << "Omp Time: " << (double) (omp_t / CLOCKS_PER_SEC) << endl;
+  cout << "Omp Speedup: " << (double)(serial_t / omp_t)
+       << endl;
+
   return 0;
 }
