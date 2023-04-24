55ef293dc8cd1c7a70e230cd9b402f064be809ca
Austin Wallace <austinwallace196@gmail.com>
2023-04-24 17:28:05 -0400

    data restructure

---
+++ cpp/src/hf_data.cpp
@@ -0,0 +1,223 @@
+#include "helper.hpp"
+#include "input.hpp"
+#include "omp.h"
+#include "stdio.h"
+#include <Eigen/Dense>
+#include <ctime>
+#include <fstream>
+#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+void HF_og() {
+  // Specify Data Path
+  /* std::string dataPath = "data/t1"; */
+  std::string dataPath = "data/t0";
+  double t1 = 1e-8, t2 = 1e-8;
+
+  // Make pointers to store input data
+  int num_atoms;
+  double E = 0, e_nuc = 0;
+  std::vector<int> *elements = nullptr;
+  std::vector<double> *eri = nullptr;
+
+  Eigen::MatrixXd *coords = nullptr;
+  Eigen::MatrixXd *T = nullptr;
+  Eigen::MatrixXd *V = nullptr;
+  Eigen::MatrixXd *e1 = nullptr;
+  Eigen::MatrixXd *S = nullptr;
+
+  // Read Data
+  input::gatherData(dataPath, num_atoms, &elements, &eri, &coords, &T, &V, &e1,
+                    &S, &e_nuc);
+  cout << "e_nuc: " << e_nuc << endl;
+
+  // Starting HF Code
+  // Allocate Memory for Matrices
+  Eigen::MatrixXd *H = nullptr;
+  Eigen::MatrixXd *S_12 = nullptr;
+  Eigen::MatrixXd *F = nullptr;
+  Eigen::MatrixXd *C = nullptr;
+  Eigen::MatrixXd *C_0_prime = nullptr;
+  Eigen::MatrixXd *D = nullptr;
+
+  // Allocate memory for energy and electron count
+  int num_electrons;
+  /* int eriSize; */
+
+  // Set Number of Electrons for a Neutral Molecule
+  helper::getNumberOfElectrons(num_atoms, elements, &num_electrons);
+
+  H = new Eigen::MatrixXd(T->rows(), T->cols());
+  *H = *T + *V;
+  cout << endl << "H Matrix: " << endl << endl << *H << endl;
+
+  // Orthogonalization of S
+  cout << endl << "S Matrix: " << endl << endl << *S << endl;
+  S_12 = new Eigen::MatrixXd(S->rows(), S->cols());
+  helper::orthoS(S, S_12);
+  cout << endl << "S_12 Matrix: " << endl << endl << *S_12 << endl;
+
+  // Build initial Fock Matrix
+  F = new Eigen::MatrixXd(H->rows(), H->cols());
+  *F = (*S_12).transpose() * *H * (*S_12);
+  cout << endl << "F Matrix: " << endl << endl << *F << endl;
+
+  C_0_prime = new Eigen::MatrixXd(H->rows(), H->cols());
+  helper::getC_0_prime(F, C_0_prime);
+  cout << endl << "C_0_prime Matrix: " << endl << endl << *C_0_prime << endl;
+
+  C = new Eigen::MatrixXd(H->rows(), H->cols());
+  *C = (*S_12) * (*C_0_prime);
+  cout << endl << "C Matrix: " << endl << endl << *C << endl;
+
+  D = new Eigen::MatrixXd(H->rows(), H->cols());
+  helper::updateDensityMatrix(C, D, num_electrons);
+  cout << endl << "D Matrix: " << endl << endl << *D << endl;
+
+  helper::SCF(eri, S_12, H, F, C, D, C_0_prime, num_electrons, &E, e_nuc, t1,
+              t2);
+  E += e_nuc;
+  cout.precision(15);
+  cout << endl << "Final HF Energy: " << E << endl;
+
+  // Final HF Energy:   -74.9659010585405
+  // Total Energy =     -74.9659900701199433
+
+  // Free Allocations
+  free(elements);
+  free(coords);
+  free(T);
+  free(V);
+  free(e1);
+  free(S);
+  free(eri);
+  printf("\nFreed Memory\n");
+}
+
+void HF(
+  int num_atoms,
+  double E = 0,
+  double e_nuc = 0,
+  std::vector<int> *elements = nullptr,
+  std::vector<double> *eri = nullptr,
+
+  Eigen::MatrixXd *coords = nullptr,
+  Eigen::MatrixXd *T = nullptr,
+  Eigen::MatrixXd *V = nullptr,
+  Eigen::MatrixXd *e1 = nullptr,
+  Eigen::MatrixXd *S = nullptr
+
+        ) {
+  // Specify Data Path
+  /* std::string dataPath = "data/t1"; */
+  std::string dataPath = "data/t0";
+  double t1 = 1e-8, t2 = 1e-8;
+
+
+  // Starting HF Code
+  // Allocate Memory for Matrices
+  Eigen::MatrixXd *H = nullptr;
+  Eigen::MatrixXd *S_12 = nullptr;
+  Eigen::MatrixXd *F = nullptr;
+  Eigen::MatrixXd *C = nullptr;
+  Eigen::MatrixXd *C_0_prime = nullptr;
+  Eigen::MatrixXd *D = nullptr;
+
+  // Allocate memory for energy and electron count
+  int num_electrons;
+  /* int eriSize; */
+
+  // Set Number of Electrons for a Neutral Molecule
+  helper::getNumberOfElectrons(num_atoms, elements, &num_electrons);
+
+  H = new Eigen::MatrixXd(T->rows(), T->cols());
+  *H = *T + *V;
+  cout << endl << "H Matrix: " << endl << endl << *H << endl;
+
+  // Orthogonalization of S
+  cout << endl << "S Matrix: " << endl << endl << *S << endl;
+  S_12 = new Eigen::MatrixXd(S->rows(), S->cols());
+  helper::orthoS(S, S_12);
+  cout << endl << "S_12 Matrix: " << endl << endl << *S_12 << endl;
+
+  // Build initial Fock Matrix
+  F = new Eigen::MatrixXd(H->rows(), H->cols());
+  *F = (*S_12).transpose() * *H * (*S_12);
+  cout << endl << "F Matrix: " << endl << endl << *F << endl;
+
+  C_0_prime = new Eigen::MatrixXd(H->rows(), H->cols());
+  helper::getC_0_prime(F, C_0_prime);
+  cout << endl << "C_0_prime Matrix: " << endl << endl << *C_0_prime << endl;
+
+  C = new Eigen::MatrixXd(H->rows(), H->cols());
+  *C = (*S_12) * (*C_0_prime);
+  cout << endl << "C Matrix: " << endl << endl << *C << endl;
+
+  D = new Eigen::MatrixXd(H->rows(), H->cols());
+  helper::updateDensityMatrix(C, D, num_electrons);
+  cout << endl << "D Matrix: " << endl << endl << *D << endl;
+
+  helper::SCF(eri, S_12, H, F, C, D, C_0_prime, num_electrons, &E, e_nuc, t1,
+              t2);
+  E += e_nuc;
+  cout.precision(15);
+  cout << endl << "Final HF Energy: " << E << endl;
+
+  // Final HF Energy:   -74.9659010585405
+  // Total Energy =     -74.9659900701199433
+
+  // Free Allocations
+  free(elements);
+  free(coords);
+  free(T);
+  free(V);
+  free(e1);
+  free(S);
+  free(eri);
+  printf("\nFreed Memory\n");
+}
+
+void timings() {
+  omp_set_num_threads(1);
+  Eigen::setNbThreads(1);
+  time_t start, end;
+  double serial_t;
+  start = clock();
+  HF_og();
+  end = clock();
+  serial_t = (double)(end - start);
+  cout << "Serial Time: " << (serial_t / CLOCKS_PER_SEC) << endl;
+  double omp_t;
+  int num_threads = 2;
+  omp_set_num_threads(num_threads);
+  Eigen::setNbThreads(num_threads);
+  start = clock();
+  HF_og();
+  end = clock();
+  omp_t = (double)(end - start);
+  cout << "Omp Time: " << (double)(omp_t / CLOCKS_PER_SEC) << endl;
+  cout << "Omp Speedup: " << (double)(serial_t / omp_t) << endl;
+}
+
+int main() {
+  /* HF_og(); */
+  /* std::string dataPath = "data/t1"; */
+  /* int num_atoms; */
+  /* double E = 0, e_nuc = 0; */
+  /* std::vector<int> *elements = nullptr; */
+  /* std::vector<double> *eri = nullptr; */
+  /*  */
+  /* Eigen::MatrixXd *coords = nullptr; */
+  /* Eigen::MatrixXd *T = nullptr; */
+  /* Eigen::MatrixXd *V = nullptr; */
+  /* Eigen::MatrixXd *S = nullptr; */
+  /* input::gatherData(dataPath, num_atoms, &elements, &eri, &coords, &T, &V, &S, */
+  /*                   &e_nuc); */
+  /* HF(num_atoms, E, e_nuc, elements, eri, coords, T, V, S); */
+    timings();
+
+
+  return 0;
+}
---
+++ cpp/src/input_data.cpp
@@ -0,0 +1,346 @@
+#include "input.hpp"
+#include "helper.hpp"
+#include <Eigen/Dense>
+#include <algorithm>
+#include <fstream>
+#include <iostream>
+#include <sstream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+void input::readVector(std::string fn, std::vector<std::vector<double>> **arr) {
+  std::ifstream file(fn);
+  if (!file) {
+    std::cout << "Could not open file: " << fn << std::endl;
+    return;
+  }
+  std::string line;
+  int count = 0;
+
+  while (getline(file, line)) {
+    count++;
+  }
+  file.clear();
+  file.seekg(0);
+
+  *arr = new std::vector<std::vector<double>>(count);
+  for (int i = 0; i < count; ++i) {
+    (*arr)->at(i) = std::vector<double>(count);
+  }
+
+  int i = 0, j;
+  while (getline(file, line)) {
+    j = 0;
+    std::stringstream ss(line);
+    std::vector<double> values;
+    double value;
+    while (ss >> value) {
+      (*arr)->at(i).at(j) = value;
+      /* std::cout << value << " "; */
+      j++;
+    }
+    /* std::cout << std::endl; */
+    i++;
+  }
+  file.close();
+  return;
+}
+
+void input::readVector(std::string fn, Eigen::MatrixXd **arr) {
+  std::ifstream file(fn);
+  if (!file) {
+    std::cout << "Could not open file: " << fn << std::endl;
+    return;
+  }
+  std::string line;
+  int count = 0;
+
+  while (getline(file, line)) {
+    count++;
+  }
+  file.clear();
+  file.seekg(0);
+
+  *arr = new Eigen::MatrixXd(count, count);
+
+  int i = 0, j;
+  while (getline(file, line)) {
+    j = 0;
+    std::stringstream ss(line);
+    std::vector<double> values;
+    double value;
+    while (ss >> value) {
+      /* std::cout << value << " " << i << " " << j << std::endl; */
+      (**arr)(i, j) = value;
+      j++;
+    }
+    /* std::cout << std::endl; */
+    i++;
+  }
+  file.close();
+  return;
+}
+void input::readVector(std::string fn, std::vector<double> **arr) {
+  std::ifstream file(fn);
+  if (!file) {
+    std::cout << "Could not open file: " << fn << std::endl;
+    return;
+  }
+  std::string line;
+  int count = 0;
+
+  while (getline(file, line)) {
+    count++;
+  }
+  file.clear();
+  file.seekg(0);
+
+  *arr = new std::vector<double>(count);
+
+  int i = 0;
+  while (getline(file, line)) {
+    std::stringstream ss(line);
+    ss >> (*arr)->at(i);
+    i++;
+  }
+  file.close();
+  return;
+}
+
+void input::readERI(std::string fn, std::vector<double> **arr, int n_basis) {
+  std::ifstream file(fn);
+  if (!file) {
+    std::cout << "Could not open file: " << fn << std::endl;
+    return;
+  }
+  std::string line;
+  int count = 0;
+
+  while (getline(file, line)) {
+    count++;
+  }
+  file.clear();
+  file.seekg(0);
+  int arrSize =
+      /* n_basis * (n_basis + 1) / 2  * (n_basis + 2) / 2 * (n_basis  + 3) / 2 ;
+       */
+      /* n_basis * n_basis * n_basis * n_basis / 8; */
+      /* helper::indexIJKL(n_basis, n_basis, n_basis, n_basis); */
+      helper::indexIJKL(n_basis - 1, n_basis - 1, n_basis - 1, n_basis - 1) + 1;
+
+  cout << "arrSize: " << arrSize << endl;
+  cout << "count: " << count << endl;
+
+  *arr = new std::vector<double>(count);
+
+  int i, j, k, l;
+  double value;
+  int ijkl;
+  while (getline(file, line)) {
+    std::stringstream ss(line);
+    ss >> i >> j >> k >> l >> value;
+    ijkl = helper::indexIJKL(i, j, k, l);
+    /* std::cout << ijkl << " " << i << " " << j << " " << k << " " << l << " "
+     */
+    /*           << value << std::endl; */
+    (*arr)->at(ijkl) = value;
+  }
+  file.close();
+  return;
+}
+
+void input::gatherData(std::string dataPath, int &num_atoms,
+                       std::vector<int> **elements, std::vector<double> **eri,
+                       std::vector<std::vector<double>> **coords,
+                       std::vector<std::vector<double>> **T,
+                       std::vector<std::vector<double>> **V,
+                       std::vector<std::vector<double>> **e1,
+                       std::vector<std::vector<double>> **overlap
+
+) {
+  std::string geom = dataPath + "/geom.xyz";
+  std::string eriFN = dataPath + "/eri.dat";
+  std::string TFN = dataPath + "/T.dat";
+  std::string VFN = dataPath + "/V.dat";
+  std::string e1FN = dataPath + "/e1.dat";
+  std::string overlapFN = dataPath + "/overlap.dat";
+  // Gathering Geometry
+  input::readGeometry(geom, num_atoms, elements, coords);
+  std::cout << "Number of atoms: " << num_atoms << std::endl;
+  input::printElements(*elements);
+  input::printVector(*coords);
+
+  // Gathering T, V, e1, overlap
+  input::readVector(TFN, T);
+  /* input::printVector(*T); */
+  input::readVector(VFN, V);
+  /* input::printVector(*V); */
+  input::readVector(e1FN, e1);
+  /* input::printVector(*e1); */
+  input::readVector(overlapFN, overlap);
+  input::readVector(eriFN, eri);
+  /* printVector(*eri); */
+}
+void input::gatherData(std::string dataPath, int &num_atoms,
+                       std::vector<int> **elements, std::vector<double> **eri,
+                       Eigen::MatrixXd **coords, Eigen::MatrixXd **T,
+                       Eigen::MatrixXd **V,
+                       Eigen::MatrixXd **overlap, double *enuc) {
+  std::string geom = dataPath + "/geom.xyz";
+  std::string eriFN = dataPath + "/eri.csv";
+  std::string TFN = dataPath + "/T.csv";
+  std::string VFN = dataPath + "/V.csv";
+  std::string overlapFN = dataPath + "/S.csv";
+  std::string enucFN = dataPath + "/enuc.dat";
+  // Gathering Geometry
+  input::readGeometry(geom, num_atoms, elements, coords);
+
+  // Gathering T, V, e1, overlap
+  input::readVector(TFN, T);
+  input::readVector(VFN, V);
+  input::readVector(overlapFN, overlap);
+
+  // Gathering eri
+  int n_basis = (*T)->rows();
+  cout << "n_basis: " << n_basis << endl;
+  input::readERI(eriFN, eri, n_basis);
+  input::readNumber(enucFN, *enuc);
+}
+
+void input::numAtoms(std::string filename, int &num_atoms) {
+  std::ifstream file(filename);
+  if (!file) {
+    std::cout << "Could not open file " << filename << std::endl;
+    return;
+  }
+  file >> num_atoms;
+}
+
+void input::gatherData(std::string dataPath, int &num_atoms,
+                       std::vector<int> **elements, std::vector<double> **eri,
+                       Eigen::MatrixXd **coords, Eigen::MatrixXd **T,
+                       Eigen::MatrixXd **V, Eigen::MatrixXd **e1,
+                       Eigen::MatrixXd **overlap, double *enuc) {
+  std::string geom = dataPath + "/geom.xyz";
+  std::string eriFN = dataPath + "/eri.dat";
+  std::string TFN = dataPath + "/T.dat";
+  std::string VFN = dataPath + "/V.dat";
+  std::string e1FN = dataPath + "/e1.dat";
+  std::string overlapFN = dataPath + "/overlap.dat";
+  std::string enucFN = dataPath + "/enuc.dat";
+  // Gathering Geometry
+  input::readGeometry(geom, num_atoms, elements, coords);
+
+  // Gathering T, V, e1, overlap
+  input::readVector(TFN, T);
+  input::readVector(VFN, V);
+  input::readVector(e1FN, e1);
+  input::readVector(overlapFN, overlap);
+
+  // Gathering eri
+  int n_basis = (*T)->rows();
+  cout << "n_basis: " << n_basis << endl;
+  input::readERI(eriFN, eri, n_basis);
+  input::readNumber(enucFN, *enuc);
+}
+
+void input::readGeometry(std::string filename, int &num_atoms,
+                         std::vector<int> **elements,
+                         Eigen::MatrixXd **coords) {
+  std::ifstream file(filename);
+  if (!file) {
+    std::cout << "Could not open file " << filename << std::endl;
+    return;
+  }
+
+  file >> num_atoms;
+  *elements = new std::vector<int>(num_atoms);
+  *coords = new Eigen::MatrixXd(num_atoms, 3);
+
+  std::string line;
+  std::getline(file, line); // read in the comment line
+
+  for (int i = 0; i < num_atoms; ++i) {
+    int el = -1;
+    double x, y, z;
+    file >> el >> x >> y >> z;
+    (*elements)->at(i) = el;
+    (**coords)(i, 0) = x;
+    (**coords)(i, 1) = y;
+    (**coords)(i, 2) = z;
+  }
+  file.close();
+  return;
+}
+
+void input::readGeometry(std::string filename, int &num_atoms,
+                         std::vector<int> **elements,
+                         std::vector<std::vector<double>> **coords) {
+  std::ifstream file(filename);
+  if (!file) {
+    std::cout << "Could not open file " << filename << std::endl;
+    return;
+  }
+
+  file >> num_atoms;
+  *elements = new std::vector<int>(num_atoms);
+  *coords = new std::vector<std::vector<double>>(num_atoms);
+  for (int i = 0; i < num_atoms; ++i) {
+    (*coords)->at(i) = std::vector<double>(3);
+  }
+
+  std::string line;
+  std::getline(file, line); // read in the comment line
+
+  for (int i = 0; i < num_atoms; ++i) {
+    int el = -1;
+    double x, y, z;
+    file >> el >> x >> y >> z;
+    (*elements)->at(i) = el;
+    (*coords)->at(i).at(0) = x;
+    (*coords)->at(i).at(1) = y;
+    (*coords)->at(i).at(2) = z;
+  }
+  file.close();
+  return;
+}
+
+void input::printVector(std::vector<std::vector<double>> *matrix) {
+  std::cout << std::endl;
+  for (int i = 0; u_int64_t(i) < matrix->size(); ++i) {
+    for (int j = 0; u_int64_t(j) < matrix->at(i).size(); ++j) {
+      std::cout.precision(12);
+      std::cout << matrix->at(i).at(j) << " ";
+    }
+    std::cout << std::endl;
+  }
+  std::cout << std::endl;
+}
+
+void input::printVector(std::vector<double> *matrix) {
+  std::cout << std::endl;
+  for (int i = 0; u_int64_t(i) < matrix->size(); ++i) {
+    std::cout.precision(12);
+    std::cout << matrix->at(i) << " ";
+  }
+  std::cout << std::endl;
+}
+
+void input::printElements(std::vector<int> *matrix) {
+  for (int i = 0; u_int64_t(i) < matrix->size(); ++i) {
+    std::cout << matrix->at(i) << " ";
+    std::cout << std::endl;
+  }
+}
+
+void input::readNumber(std::string filename, double &number) {
+  std::ifstream file(filename);
+  if (!file) {
+    std::cout << "Could not open file " << filename << std::endl;
+    return;
+  }
+  file >> number;
+  file.close();
+}
