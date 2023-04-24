5fa620a86432a03918459cc5d86bdd84b6ba226a
Austin Wallace <austinwallace196@gmail.com>
2023-04-24 14:27:22 -0400

    t2

--- psi4/main.py
+++ psi4/main.py
@@ -5,24 +5,42 @@ from qm_tools_aw import tools
 import qcelemental as qcel
 
 
-def psi4_compute(mol):
+def psi4_compute(mol, outdata="t2"):
     psi4.set_memory('2 GB')
-    psi4.set_num_threads(2)
+    psi4.set_num_threads(10)
     psi4.core.set_output_file('output.dat', False)
     psi4.set_options({"basis": "aug-cc-pvdz"})
     wfn = psi4.core.Wavefunction.build(mol,
                                        psi4.core.get_global_option("basis"))
     mints = psi4.core.MintsHelper(wfn.basisset())
     S = np.asarray(mints.ao_overlap())
-    print(f"{S = }")
+    np.savetxt(f"{outdata}/S.csv", S, delimiter=" ")
+    # print(f"{S = }")
     T = np.asarray(mints.ao_potential())
-    print(f"{T = }")
+    np.savetxt(f"{outdata}/T.csv", T, delimiter=" ")
+    # print(f"{T = }")
     V = np.asarray(mints.ao_kinetic())
-    print(f"{V = }")
+    np.savetxt(f"{outdata}/V.csv", V, delimiter=" ")
+    # print(f"{V = }")
     I = np.asarray(mints.ao_eri())
-    print(f"{I = }")
+    nbf = len(I)
+    print(f"{nbf = }")
 
-def find_geoms() -> None:
+    with open(f"{outdata}/eri.csv", 'w') as f:
+        for i in range(nbf):
+            for j in range(i + 1):
+                for k in range(i + 1):
+                    for l in range(k + 1):
+                        # print(i, j, k, l, I[i,j,k,l])
+                        line = f"{i} {j} {k} {l} {I[i,j,k,l]}\n"
+                        # line = f"{i} {j} {k} {l}\n"
+                        f.write(line)
+
+    e = psi4.energy("HF/aug-cc-pvdz")
+    print(f"{e =}")
+    return
+
+def find_geoms(size=10) -> None:
     """
     find_geoms
     """
@@ -35,13 +53,13 @@ def find_geoms() -> None:
     df = pd.read_pickle("schr.pkl")
     print(df)
     for n, r in df.iterrows():
-        if len(r['monAs']) == 10:
+        if len(r['monAs']) == size:
             mmA = r['Geometry'][r['monAs'], :]
             return tools.print_cartesians_pos_carts(mmA[:,0], mmA[:,1:])
 
 def main():
     # mol = qcel.models.Molecule.from_file("t1/t1.xyz")
-    d = find_geoms()
+    d = find_geoms(6)
     # fn = "t1/t1.xyz"
     # with open(fn, "r") as f:
     #     d = "".join(f.readlines()[2:])
